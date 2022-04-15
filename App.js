3e4497de261322f9d1b9b182c68e9e5519a8311b
Nga Pham <ngapham3108@gmail.com>
2022-04-15 01:33:19 -0700

    add info

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ const { Server } = require('ws');
 const PORT = process.env.PORT || 3000;
 const server = express()
   .use((req, res) => {
-    const remoteIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const xForwardedFor = req.headers['x-forwarded-for'];
+    const connectionRemoteAddress = req.connection.remoteAddress;
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end(`<!DOCTYPE html>
@@ -20,13 +21,16 @@ const server = express()
     </head>
 
     <body>
-        <h1>HTTPS req ip: ${remoteIP}<h1/>
-        <h1 id="wss"><h1/>
+        <h1>HTTPS x-forwarded-for: ${xForwardedFor}<h1/>
+        <h1>HTTPS remoteAddress: ${connectionRemoteAddress}<h1/>
+        <h1 id="content1"><h1/>
+        <h1 id="content2"><h1/>
         <script>
             let HOST = location.origin.replace(/^http/, 'ws')
             let ws = new WebSocket(HOST);
             ws.onmessage = (event) => {
-                document.getElementById("wss").textContent = 'WSS req ip: ' + event.data;
+                let [id, value] = event.data.split('*');
+                document.getElementById(id).textContent = value;
             };
         </script>
     </body>
@@ -39,7 +43,9 @@ const wss = new Server({ server });
 
 wss.on('connection', (ws, req) => {
   console.log('Client connected');
-  const remoteIP = req.headers['x-forwarded-for'] || ws._socket.remoteAddress;
-  ws.send(remoteIP);
+  const xForwardedFor = req.headers['x-forwarded-for'];
+  const socketRemoteAddress = ws._socket.remoteAddress;
+  ws.send('content1*WSS x-forwarded-for: ' + xForwardedFor);
+  ws.send('content2*WSS remoteAddress: ' + socketRemoteAddress);
   ws.on('close', () => console.log('Client disconnected'));
 });
